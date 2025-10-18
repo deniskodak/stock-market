b7745510f3e1464d91107e829a9a36bc64d4444a
Denys Kodak <dekodak@deloittece.com>
2025-10-18 10:49:13 +0200

    Add header component && add ui Avatar, DropdownMenu

---
+++ components/NavItems.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { NAV_ITEMS } from "@/lib/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FunctionComponent } from "react";
+
+interface NavItemsProps {}
+
+const NavItems: FunctionComponent<NavItemsProps> = () => {
+  const pathname = usePathname();
+
+  const isActive = (href: string) => {
+    if (href === '/') return pathname === '/';
+
+    return pathname.startsWith(href);
+  }
+
+  return (
+    <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 text-md font-medium">
+      {NAV_ITEMS.map((item) => (
+        <li key={item.label}>
+          <Link href={item.href} className={`hover:text-yellow-500 transition-colors ${
+            isActive(item.href) ? "text-gray-100" : ""
+          }`}>
+            {item.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavItems;
---
+++ components/UserDropdown.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { FunctionComponent } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import UserAvatar from "./UserAvatar";
+import { LogOut } from "lucide-react";
+import NavItems from "./NavItems";
+
+interface UserDropdownProps {}
+
+const UserDropdown: FunctionComponent<UserDropdownProps> = () => {
+  const router = useRouter();
+  const userData = {
+    name: "John",
+    email: "signalist.contact@gmail.com",
+  };
+
+  const handleSignOut = () => {
+    router.push("/sign-in");
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex items-center gap-3 text-gray-400 hover:text-yellow-500"
+        >
+          <UserAvatar customClassName="h-8 w-8" userName={userData.name} />
+
+          <div className="hidden md:flex flex-col items-start">
+            <span className="text-base font-medium text-gray-400">
+              {userData.name}
+            </span>
+          </div>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>
+          <div className="flex relative items-center gap-3 p-2">
+            <UserAvatar customClassName="h-10 w-10" userName={userData.name} />
+            <div className="flex flex-col">
+              <span className="text-base font-medium text-gray-400">
+                {userData.name}
+              </span>
+              <span className="text-sm font-medium text-gray-500">
+                {userData.email}
+              </span>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator className="bg-gray-600" />
+        <DropdownMenuItem
+          onClick={handleSignOut}
+          className="text-gray-100 text-medium focus:bg-transparent focus:text-yellow-500 transition-colors"
+        >
+          <LogOut className="mr-2 h-4 w-4 hidden sm:block" />
+          Logout
+        </DropdownMenuItem>
+        <DropdownMenuSeparator className="hidden sm:block bg-gray-600" />
+        <nav aria-label="Main" className="sm:hidden">
+          <NavItems />
+        </nav>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserDropdown;
